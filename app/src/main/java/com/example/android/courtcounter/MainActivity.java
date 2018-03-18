4a098fad98f5cd4566b4c84a0771221d603ddd34
Elizabeth Nelson <17551572+ElizabethLane@users.noreply.github.com>
2018-03-17 20:59:53 -0700

    Added on click event methods

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
     }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeTeamA(View v){
+        displayForTeamA(3);
+    }
+    public void addTwoTeamA(View v){
+        displayForTeamA(3);
+    }
+    public void addOneTeamA(View v){
+        displayForTeamA(3);
+    }
+
 }
