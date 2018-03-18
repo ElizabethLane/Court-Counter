b172169a7d08570527144ad9b95451c43d2c431f
Elizabeth Nelson <17551572+ElizabethLane@users.noreply.github.com>
2018-03-17 21:04:12 -0700

    Updated methods to display global variable score

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -13,22 +13,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         displayForTeamA(8);
     }
+
+    int TeamAScore = 0;
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        score = TeamAScore;
         scoreView.setText(String.valueOf(score));
     }
 
     public void addThreeTeamA(View v){
-        displayForTeamA(3);
+        displayForTeamA(TeamAScore);
     }
     public void addTwoTeamA(View v){
-        displayForTeamA(3);
+        displayForTeamA(TeamAScore);
     }
     public void addOneTeamA(View v){
-        displayForTeamA(3);
+        displayForTeamA(TeamAScore);
     }
 
 }
